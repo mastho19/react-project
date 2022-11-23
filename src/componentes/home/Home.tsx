9a1a8224f9103ad9cb85b7a3eeb0d3ecfc4c7be3
mastho19 <thom.almeida19@gmail.com>
2022-11-23 13:02:37 -0300

    first commit

---
+++ src/componentes/home/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './home.css'
+
+function Home () {
+
+    return(
+        <>
+            <h1 className="titulo">Comidas Típicas</h1>
+
+            <div className="comida-tipica">
+            <img src="https://www.dellmare.com.br/wp-content/uploads/2021/11/Acaraje-de-Camarao-1-1-scaled.jpg" alt="" className="img"/>
+            </div>
+        </>
+    )
+}
+
+export default Home
---
+++ src/componentes/navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './navbar.css'
+
+function Navbar () {
+    return(
+        <>
+            <nav className="menu">
+                <a href="" className="link">Home</a>
+                <a href="" className="link">Sobre</a>
+                <a href="" className="link">Catálogo</a>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
